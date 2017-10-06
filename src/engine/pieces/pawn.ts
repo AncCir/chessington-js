7ed96cb3dbb5bfd49bd54ed33d5da903ee6acd6a
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 12:39:53 +0100

    Made prawns move, test 1

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,28 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let location : Square = board.findPiece(this);
+        let availableMoves = [];
+        //verificam daca playerul este alb
+        if (this.player === Player.WHITE) {
+            if (location.row === 1) {
+                //se poate muta atat un rand, cat si doua in fata
+                availableMoves.push(new Square(location.row + 1, location.col));
+                availableMoves.push(new Square(location.row + 2, location.col));
+            } else {
+                // se poate muta doar un rand in fata
+                availableMoves.push(new Square(location.row + 1, location.col));
+            }
+        } else {
+            if (location.row === 6) {
+                //se poate muta atat un rand, cat si doua in fata
+                availableMoves.push(new Square(location.row - 1, location.col));
+                availableMoves.push(new Square(location.row - 2, location.col));
+            } else {
+                // se poate muta doar un rand in fata
+                availableMoves.push(new Square(location.row - 1, location.col));
+            }
+        }
+        return availableMoves;
     }
 }
